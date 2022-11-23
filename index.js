fde92469afb3ec945692cf198272d3300bcde632
kawai011231 <yousuke011231@gmail.com>
2022-11-23 16:32:04 +0900

    例外処理のタブーサーチ

--- index.js
+++ index.js
@@ -11,14 +11,38 @@ reader.on("line", (line) => {
   lines.push(line);
 });
 reader.on("close", () => {
-  const n = Number(lines[0].split(" ")[0]);
-  const l = Number(lines[0].split(" ")[1]);
-  const ary = lines[n + 1].split(" ");
-  for (let i = 0; i < n; i++) {
-    let a = lines[i + 1].split(" ");
-    if (ary.toString() == a.toString()) {
-      console.log(i + 1);
+  const input = lines[0].split(" ");
+  const n = Number(input[0]);
+  const m = Number(input[1]);
+  const l = Number(input[2]);
+  const pal = [];
+  const tim = [];
+  for (let i = 1; i <= m; i++) {
+    let a = lines[i].split(" ");
+    let b = [];
+    for (let j = 0; j < a.length; j++) {
+      b.push(Number(a[j]));
+    }
+    pal.push(b);
+  }
+  for (let i = n + 1; i <= l + 3; i++) {
+    let a = lines[i].split(" ");
+    let b = [];
+    for (let j = 0; j < a.length; j++) {
+      b.push(Number(a[j]));
+    }
+    tim.push(b);
+  }
+  for (let i = 0; i < l - 1; i++) {
+    let dis = [];
+    for (let j = 0; j < n; j++) {
+      let x = tim[i + 1][j] - tim[i][j];
+      dis.push(x);
+    }
+    for (let j = 0; j < m; j++) {
+      if (dis.toString() === pal[j].toString()) {
+        console.log(j + 1);
+      }
     }
   }
 });
-// 配列が同じかどうか比較するときはtoString()関数を使う
