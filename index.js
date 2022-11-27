ae183c7bab1194e9c1cdaf728238d345f14832ec
kawai011231 <yousuke011231@gmail.com>
2022-11-27 16:05:08 +0900

    ダンジョンのデッドロック 1修正

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 process.stdin.resume();
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding("utf8");
 // 自分の得意な言語で
 // Let's チャレンジ！！
 var lines = [];
-var reader = require('readline').createInterface({
+var reader = require("readline").createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
-reader.on('line', (line) => {
+reader.on("line", (line) => {
   lines.push(line);
 });
-reader.on('close', () => {
-    const room=Number(lines[0].split(' ')[0])
-    const hum=Number(lines[0].split(' ')[1])
-    const time=Number(lines[0].split(' ')[2])
-    let full=[]
-    for(let i=0;i<hum;i++){
-        full.push(Number(lines[i+1]))
+reader.on("close", () => {
+  const room = Number(lines[0].split(" ")[0]);
+  const hum = Number(lines[0].split(" ")[1]);
+  const time = Number(lines[0].split(" ")[2]);
+  let full = [];
+  for (let i = 0; i < hum; i++) {
+    full.push(Number(lines[i + 1]));
+  }
+  for (let i = 1; i <= time; i++) {
+    let a = Number(lines[i + hum].split(" ")[0]);
+    let b = Number(lines[i + hum].split(" ")[1]);
+    console.log(a);
+    console.log(b);
+    if (Number(lines[i]) === b) {
+      console.log("Yes");
+    } else if (full.includes(b)) {
+      console.log("No");
+    } else {
+      console.log("Yes");
     }
-    for(let i=hum+1;i<time+hum+1;i++){
-        let a=Number(lines[i].split(' ')[0])
-        let b=Number(lines[i].split(' ')[1])
-        if(Number(lines[i-hum])===b){
-            console.log("Yes")
-        }else if(full.includes(b)){
-            console.log("No")
-        }else{
-            console.log("Yes")
-        }
-    }
+  }
 });
